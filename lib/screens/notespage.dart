3d385b823879a6d7f8fa75054671ad1d97ebc91c
ItsBarthromew <barthromewraps@gmail.com>
2025-03-27 23:25:35 +0200

    Refactor ChatScreen and NotesPage layout for improved UI consistency and readability

--- lib/screens/notespage.dart
+++ lib/screens/notespage.dart
@@ -1,3 +1,4 @@
+import 'package:addiction_aider/consts/colors.dart';
 import 'package:flutter/material.dart';
 
 class Notespage extends StatelessWidget {
@@ -28,70 +29,78 @@ class Notespage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: const Color(0xFFDCD9EC),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF28E07E),
-        title: const Text(
-          'My Notes',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            fontFamily: "Fatone",
-            color: Colors.black,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: notes.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 12.0),
-              child: Container(
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: const Color(0xFF28E07E)),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      notes[index]['title']!,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        fontFamily: "Fatone",
-                        color: Colors.black,
-                      ),
+      
+      body: Column(
+        children: [
+          const Text(
+                    'My Notes',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Fatone",
+                      color: Colors.black,
                     ),
-                    const SizedBox(height: 4),
-                    Text(
-                      notes[index]['summary']!,
-                      style: const TextStyle(fontSize: 14, color: Colors.black),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(height: 4),
-                    InkWell(
-                      onTap: () {},
-                      child: const Text(
-                        'Read More',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF28E07E),
+                  ),
+                  const SizedBox(height: 20,),
+          SizedBox(
+            height: 527,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView.builder(
+                itemCount: notes.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: mainColor,
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: const Color(0xFF28E07E)),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                notes[index]['title']!,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontFamily: "Baloo",
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                notes[index]['summary']!,
+                                style: const TextStyle(fontSize: 14, color: Colors.black),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(height: 4),
+                              InkWell(
+                                onTap: () {},
+                                child: const Text(
+                                  'Read More',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF28E07E),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: const Color(0xFF28E07E),
